133ba6f81aa8c55a41ed242225417c3a7d6970ee
ck2lucky <ck2lucky@naver.com>
2021-09-10 11:18:56 +0900

    .

---
+++ svg/svg.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", function() {
+
+    var example = document.querySelector("#svgExample").contentDocument;
+    var popup = document.querySelector("#popup");
+    example.addEventListener('mouseover', on);
+    example.addEventListener('mouseout', onout);
+
+    function on(e){
+        e.target.classList.add("on");
+        if(e.target.classList.contains("st0")) {
+            popup.style.display = "block";
+            popup.children[0].innerHTML = "안녕하세요";
+        }
+        if(e.target.classList.contains("st1")) {
+            popup.style.display = "block";
+            popup.children[0].innerHTML = "방가방가";
+        }
+        if(e.target.classList.contains("st1")) {
+            popup.style.display = "block";
+            popup.children[0].innerHTML = "굿모닝";
+        }
+        if(e.target.classList.contains("st1")) {
+            popup.style.display = "block";
+            popup.children[0].innerHTML = "식사하셨습니까?";
+        }
+        if(e.target.classList.contains("st1")) {
+            popup.style.display = "block";
+            popup.children[0].innerHTML = "잘가요~~";
+        }
+
+    }
+    function onout(e){
+        e.target.classList.remove("on");
+        popup.style.display = "none";
+    }
+
+
+});
